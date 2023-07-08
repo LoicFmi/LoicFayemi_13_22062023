b39b0adc27047d9d6ae6dd069a47846a200e2c05
LoicFmi <90246399+LoicFmi@users.noreply.github.com>
2023-07-08 09:33:31 +0200

    Addes Backend

---
+++ argent_bank/src/store.js
@@ -0,0 +1,9 @@
+// import { createStore } from 'redux';
+// import { useDispatch } from 'react-redux';
+// import { useSelector } from 'react-redux';
+// import produce from 'immer';
+// import { configureStore } from '@reduxjs/toolkit'
+
+// const reduxDevtools =
+//   window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__();
+// const store = createStore(reducer, reduxDevtools);
